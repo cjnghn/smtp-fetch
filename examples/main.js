311fcc7839c26499eccf035d6277e5ef05ecc82f
Reeul <chotnt741@gmail.com>
2023-02-28 11:49:06 +0900

    add verify

--- examples/main.js
+++ examples/main.js
@@ -4,7 +4,7 @@ const GMAIL_SMTP = "alt2.gmail-smtp-in.l.google.com";
 const SMTP_PORT = 25;
 
 async function main() {
-  const client = new Client(GMAIL_SMTP, SMTP_PORT, "localhost");
+  const client = new Client(GMAIL_SMTP, SMTP_PORT);
   await client.connect();
 
   console.log(await client.ehlo());
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const { SMTPDisconnected } = require("./error");
 
 class Client {
 
-  constructor(host, port, localName) {
+  constructor(host, port, localName='localhost') {
     this.socket = null;
     this.ext = {};
 
@@ -21,7 +21,7 @@ class Client {
       this.socket.on("connect", () => {
         this.socket.once("data", (data) => {
           debug("connect success: " + data.toString());
-          resolve();
+          resolve(this._parseCodeLine(data.toString(), 220));
         });
       });
 
@@ -47,12 +47,18 @@ class Client {
     }
     const line = util.format(`${format}\r\n`, ...args);
     return new Promise((resolve, reject) => {
-      debug('write: ' + line)
+      debug("write: " + line);
       this.socket.write(line, "utf-8", () => {
         this.socket.once("data", (data) => {
           resolve(this._parseCodeLine(data.toString(), expectCode));
         });
       });
+    });
+  }
+
+  verify(addr) {
+    return new Promise((resolve, reject) => {
+      this.cmd(250, "VRFY %s", addr).then(resolve).catch(reject);
     });
   }
 
@@ -80,10 +86,8 @@ class Client {
 
   quit() {
     return new Promise((resolve, reject) => {
-      this.cmd(221, "QUIT")
-        .then(resolve)
-        .catch(reject);
-    })
+      this.cmd(221, "QUIT").then(resolve).catch(reject);
+    });
   }
 
   _parseCodeLine(reply, expectCode) {
@@ -98,7 +102,7 @@ class Client {
       }
     }
 
-    return { code, message: resp.join('\n') };
+    return { code, message: resp.join("\n") };
   }
 }
 
